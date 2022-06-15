c98bb9f59ced2c1fc7407d6a3d2360f746474653
flizzyflo <vip3r1@gmx.de>
2022-06-15 12:25:54 +0200

    updated path selection with os module

--- MoveFile.py
+++ MoveFile.py
@@ -3,40 +3,48 @@ import shutil
 import os
 from PyPDF2 import PdfFileReader
 
+
+"""Path input; serves as source folder for the initial read in and as destination folder
+where the (renamed) files should be transferred to."""
+
 destination_path = r"C:\Users\flori\Desktop\Downloads sortiert"
 original_path = r"C:\Users\flori\Downloads"
 
 skippable_file_suffix = [".ini"]
 
 
-def read_information(filename: str, filepath: str) -> tuple[str, str]:
+def get_pdf_file_metainformation(filepath: str, filename: str) -> tuple[str, str]:
         """Helper Function to extract meta data of PDF files"""
-        
+
         try:
-            with open(f"{filepath}\{filename}", "rb") as file:
+            with open(f"{os.path.join(filepath, filename)}", "rb") as file:
                 pdf_file_information = PdfFileReader(file).getDocumentInfo()
                 pdf_title = pdf_file_information["/Title"]
                 pdf_author = pdf_file_information["/Author"]
 
-            return tuple(pdf_title, pdf_author)
+            return (pdf_title, pdf_author)
         
         except:
-            return tuple("No title found in metadata", "No Author found in Metadata")
+            return ("No title found in metadata", "No author found in metadata")
 
 
-def rename_pdf_file(filename: Path, filepath: str) -> None:
+def rename_pdf_file(filepath: str, filename: Path) -> None:
     """Function to rename PDF files according to their stored Meta data, namely Title and Author"""
     
-    pdf_title, pdf_author = read_information(filename, filepath)
+    pdf_title, pdf_author = get_pdf_file_metainformation(filepath, filename)
 
-    os.rename(f"{original_path}\{filename}", f"{original_path}\{pdf_author} - {pdf_title}.pdf")
+    try:
+        os.rename(os.path.join(original_path, filename), os.path.join(original_path, f"{pdf_author} - {pdf_title}.pdf" ))
+    
+    except: 
+        os.rename(os.path.join(original_path, filename), os.path.join(original_path, f"{pdf_author} - {pdf_title} - copy.pdf" ))
 
 
 def move_pdf_file(file: Path, source_path: str, destination_path: str) -> None:
     """Function to move .pdf file from source path to destination path"""
 
     try:
-        shutil.move(src= source_path, dst= destination_path)
+        shutil.move(src= os.path.join(source_path, file.name), dst= destination_path)
 
     except:
         print(f"'{file}' already exists in '{destination_path}'")
@@ -47,7 +55,7 @@ def move_exe_files(file: Path, source_path: str, destination_path: str) -> None:
 
     try:
         if file.suffix.lower() == ".exe":
-            shutil.move(src= source_path, dst= destination_path)
+            shutil.move(src= os.path.join(source_path, file.name), dst= destination_path)
     except:
         print(f"'{file}' already exists in '{destination_path}'")
 
@@ -57,7 +65,7 @@ def move_zip_files(file: Path, source_path: str, destination_path: str) -> None:
 
     try:
         if file.suffix.lower() == ".zip":
-            shutil.move(src= source_path, dst= destination_path)
+            shutil.move(src= os.path.join(source_path, file.name), dst= destination_path)
     except:
         print(f"'{file}' already exists in '{destination_path}'")
 
@@ -65,10 +73,10 @@ def move_zip_files(file: Path, source_path: str, destination_path: str) -> None:
 ####################################################################################
 
 target_folders = os.listdir(destination_path)
+print
 
 for file in os.listdir(original_path):
     f = Path(file)
-    print(os.listdir(original_path))
     
     filename, filesuffix = f.name, f.suffix
 
@@ -77,8 +85,8 @@ for file in os.listdir(original_path):
 
     if filesuffix == ".pdf":
 
-        rename_pdf_file(f, original_path)
-        move_pdf_file(f, f"{original_path}\{filename}", fr"{destination_path}\{target_folders[1]}")
+        rename_pdf_file(original_path, f)
+        move_pdf_file(f, original_path, fr"{destination_path}\{target_folders[1]}")
 
-    # move_exe_files(f, f"{original_path}\{filename}", fr"{destination_path}\{target_folders[0]}")
-    # move_zip_files(f, f"{original_path}\{filename}", fr"{destination_path}\{target_folders[2]}")
+    move_exe_files(f, original_path, fr"{destination_path}\{target_folders[0]}")
+    # move_zip_files(f, original_path, fr"{destination_path}\{target_folders[2]}")
